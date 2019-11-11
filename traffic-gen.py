d62d7b205aae5d92452fabc83c91041472d606bb
fabio <fabiofanni@users.noreply.github.com>
2019-11-10 22:21:26 -0800

    Create traffic-gen.py

---
+++ traffic-gen.py
@@ -0,0 +1,31 @@
+import selenium as se
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from fake_useragent import UserAgent
+import time
+import random
+import csv
+
+url = input("Enter destination URL: ")
+visits = int(input("Enter number of visits: "))
+
+options = Options()
+ua = UserAgent()
+
+for i in range(visits):
+    userAgent = ua.random
+    print(userAgent)
+    options.add_argument(f'user-agent={userAgent}')
+    driver = webdriver.Chrome(chrome_options=options, executable_path=r'/Users/nomad/Downloads/chromedriver')
+    driver.get(url)
+    driver.set_window_size(1120, 550)
+    time.sleep(random.randint(2,10)) 
+    print(driver.current_url)
+    print(i)
+    with open('traffic3.log', 'a') as f:
+        writer = csv.writer(f)
+        writer.writerow(["User Agent"])
+        writer.writerow(userAgent)
+        #writer.writerows(int(time.time()))
+        #writer.writerow(int(i))
+driver.quit()
